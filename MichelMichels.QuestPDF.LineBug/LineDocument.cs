4fcc0d6ca0ca537569335060efbbe4f21993a8a7
Michel Michels <michel.michels.jr@gmail.com>
2022-02-18 11:09:58 +0100

    Text spacing issue

--- MichelMichels.QuestPDF.LineBug/LineDocument.cs
+++ MichelMichels.QuestPDF.LineBug/LineDocument.cs
@@ -24,6 +24,15 @@ namespace MichelMichels.QuestPDF.LineBug
 
         private void ComposeHeader(IContainer container)
         {
+            container.Column(column =>
+            {
+                column.Item().Element(ComposeLineBug);
+                column.Item().Element(ComposeTextSpacing);
+            });
+        }
+
+        void ComposeLineBug(IContainer container)
+        {
             container.Row(row =>
             {
                 row
@@ -43,11 +52,29 @@ namespace MichelMichels.QuestPDF.LineBug
                         column.Item().Text("Line 2");
 
                         // This makes the code go boom
-                        column.Item().LineHorizontal(0.1f);
+                        //column.Item().LineHorizontal(0.1f);
 
                         // Following line doesn't crash the program:
                         //
-                        // column.Item().LineHorizontal(0.1f, Unit.Millimetre);
+                        column.Item().LineHorizontal(0.1f, Unit.Millimetre);
+                    });
+
+            });
+        }
+
+        void ComposeTextSpacing(IContainer container)
+        {
+            container.Row(row =>
+            {
+                row
+                    .RelativeItem()
+                    .Column(column =>
+                    {
+                        column.Item().Text("Text without spaces in front");
+                        column.Item().Text("  Text with 2 spaces");
+                        column.Item().Text("    Text with 4 spaces");
+                        column.Item().Text("\tText with 1 tab");
+                        column.Item().Text("\t\tText with 2 tabs");
                     });
             });
         }
