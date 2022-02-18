26febf01530c316f50d9cb96a5eda1274915417b
Michel Michels <michel.michels.jr@gmail.com>
2022-02-18 09:56:13 +0100

    Initial commit

---
+++ MichelMichels.QuestPDF.LineBug/LineDocument.cs
@@ -0,0 +1,55 @@
+﻿using QuestPDF.Drawing;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MichelMichels.QuestPDF.LineBug
+{
+    public class LineDocument : IDocument
+    {
+        public void Compose(IDocumentContainer container)
+        {
+            container.Page(page =>
+            {
+                page.Header().Element(ComposeHeader);
+            });
+        }
+
+        public DocumentMetadata GetMetadata() => DocumentMetadata.Default;
+
+        private void ComposeHeader(IContainer container)
+        {
+            container.Row(row =>
+            {
+                row
+                    .RelativeItem()
+                    .Background(Colors.Blue.Accent1)
+                    .Column(column =>
+                    {
+                        column.Item().Text("Line 1");
+                    });
+
+                row
+                    .RelativeItem()
+                    .Background(Colors.Red.Accent1)
+                    .Column(column =>
+                    {
+                        column.Item().Text("Line 1");
+                        column.Item().Text("Line 2");
+
+                        // This makes the code go boom
+                        column.Item().LineHorizontal(0.1f);
+
+                        // Following line doesn't crash the program:
+                        //
+                        // column.Item().LineHorizontal(0.1f, Unit.Millimetre);
+                    });
+            });
+        }
+    }
+}
---
+++ MichelMichels.QuestPDF.LineBug/Program.cs
@@ -0,0 +1,8 @@
+﻿using MichelMichels.QuestPDF.LineBug;
+using QuestPDF.Fluent;
+
+var document = new LineDocument();
+document.GeneratePdf("line.pdf");
+
+Console.WriteLine("Done.");
+
